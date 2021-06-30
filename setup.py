13c490f782006946ace31233418aa4881ea8ef8c
老张 <360014296@qq.com>
2021-06-30 20:45:28 +0800

    version 0.0.1

--- setup.py
+++ setup.py
@@ -6,27 +6,25 @@ with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setuptools.setup(
-    name="cgai_socket",
-    version="0.0.5",
+    name="cgai_io",
+    version="0.0.1",
     author="Master Zhang",
     author_email="360014296@qq.com",
-    description="A simple and fast library supporting custom callback function.",
+    description="A simple, light and fast data stream operation Python Library.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/zxzxde/cgai-socket",
+    url="https://github.com/zxzxde/cgai-io",
     packages=setuptools.find_packages(),
     classifiers=[
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 2.8",
-        "Programming Language :: Python :: 2.9",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8"
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9"
     ],
 )
